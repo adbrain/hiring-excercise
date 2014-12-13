c5753a28fe5f6dab09c53da7275e708d0303b282
Panos <codiply@gmail.com>
2014-12-13 12:30:09 +0000

    Added project for DataAccess layer and class for person node for binary tree .

---
+++ functional-tree-traversal/FunctionalTree/DataAccess/Entities/PersonNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adbrain.DataAccess.Entities
+{
+    public class PersonNode
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int? LeftChildId { get; set; }
+
+        public int? RightChildId { get; set; }
+
+        public PersonNode LeftChild { get; set; }
+
+        public PersonNode RightChild { get; set; }
+    }
+}
