f3401b746bb95457f420c0c66fcf3ef94f4f189e
Panos <codiply@gmail.com>
2014-12-13 12:17:06 +0000

    Added PeopleController with get and post action. Work in progress.

---
+++ functional-tree-traversal/FunctionalTree/WebApi/Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        // GET api/people/5
+        public string Get(string name, int age)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/people
+        public void Post(string name, int age)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
