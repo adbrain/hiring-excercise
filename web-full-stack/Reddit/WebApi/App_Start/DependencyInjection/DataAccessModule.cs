8796445439cfad9fb248ea1bce88f17fb391496c
Panos <codiply@gmail.com>
2014-12-17 20:17:11 +0000

    Installed Ninject.

---
+++ web-full-stack/Reddit/WebApi/App_Start/DependencyInjection/DataAccessModule.cs
@@ -0,0 +1,22 @@
+﻿using Adbrain.DataAccess.DbContexts;
+using Adbrain.DataAccess.Repositories;
+using Adbrain.DataAccess.Wrappers;
+using Ninject.Modules;
+using Ninject.Web.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adbrain.WebApi.App_Start.DependencyInjection
+{
+    public class DataAccessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IClock>().To<SystemClock>().InSingletonScope();
+            Bind<ISqlDbContext>().To<SqlDbContext>().InRequestScope();
+            Bind<IRawDataRepository>().To<RawDataRepository>().InRequestScope();
+        }
+    }
+}
---
+++ web-full-stack/Reddit/WebApi/App_Start/DependencyInjection/WebApiModule.cs
@@ -0,0 +1,15 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adbrain.WebApi.App_Start.DependencyInjection
+{
+    public class WebApiModule : NinjectModule
+    {
+        public override void Load()
+        {
+        }
+    }
+}
