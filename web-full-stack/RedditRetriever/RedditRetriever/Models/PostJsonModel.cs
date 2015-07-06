968b0b1a60fe5eef6a0794ab8966cb3f6b675733
Anthony Brown <bruinbrown@gmail.com>
2015-07-06 20:56:33 +0100

    Modeling of Json key value data in DB

---
+++ web-full-stack/RedditRetriever/RedditRetriever/Models/PostJsonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RedditRetriever.Models
+{
+    public class PostJsonModel
+    {
+        [Key]
+        public string Key { get; set; }
+        public string Json { get; set; }
+    }
+}
