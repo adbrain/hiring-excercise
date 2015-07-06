38f9222c0d585fb272b1f8e3a335fb3682040c20
Anthony Brown <bruinbrown@gmail.com>
2015-07-06 22:36:32 +0100

    Updated controller to use repository

--- web-full-stack/RedditRetriever/RedditRetriever/Controllers/RedditController.cs
+++ web-full-stack/RedditRetriever/RedditRetriever/Controllers/RedditController.cs
@@ -2,6 +2,7 @@
 using RedditRetriever.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,37 +13,28 @@ namespace RedditRetriever.Controllers
 {
     public class RedditController : ApiController
     {
-        private const string BaseUrl = "/r/sports.json?limit=100";
         private readonly IRedditAccess _reddit;
+        private readonly IRedditRepository _repository;
 
         public RedditController()
-            : this(new RedditAccess())
+            : this(new RedditAccess(), new RedditRepository())
         {
         }
 
-        public RedditController(IRedditAccess reddit)
+        public RedditController(IRedditAccess reddit, IRedditRepository repository)
         {
             _reddit = reddit;
-        }
-
-        private string RetrieveUrl(string domain)
-        {
-            if(domain == null)
-            {
-                return BaseUrl;
-            }
-            var completeDomain = BaseUrl + "&site=" + domain;
-            return completeDomain;
+            _repository = repository;
         }
 
         [HttpGet]
         [Route("sports")]
-        public async Task<IEnumerable<Post>> GetSubreddit()
+        public async Task<IEnumerable<IGrouping<string, Post>>> GetSubreddit(string domain)
         {
-            var domain = Request.GetQueryNameValuePairs().SingleOrDefault(x => x.Key == "domain").Value;
-            var url = RetrieveUrl(domain);
-            var res = await _reddit.GetUrlAsync(url);
-            return res;
+            var url = _reddit.RetrieveUrlWithDomain(domain);
+            var posts = await _reddit.GetPathAsync(url);
+            await _repository.SavePostsAsync(posts);
+            return posts.GroupBy(x => x.Author);
         }
     }
 }
